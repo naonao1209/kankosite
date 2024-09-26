8b2193a506c3aa44088d4cbf82eb41d7824aec9d
naonao1209 <naotaka9651@gmail.com>
2024-09-26 23:24:04 +0900

    settings.pyの更新

--- kanko/settings.py
+++ kanko/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-from .local_settings import *
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
